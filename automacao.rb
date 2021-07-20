c860d817cae5d1ab0d107fa88fe0ffd029afd5f3
Rtristao <78371478+Rtristao@users.noreply.github.com>
2021-07-20 14:51:32 -0300

    Add files via upload

---
+++ automacao.rb
@@ -0,0 +1,16 @@
+source 'https://rubygems.org'
+
+require 'cucumber'
+require 'capybara'
+require 'rspec'
+require 'selenium-webdriver'
+
+# Feature: Register user
+#     As a new user
+#     I'd like to register myself
+#     So i'll buy a product
+
+# Scenario: Register a new user
+#     Given i'm at register screen
+#     When 
+
